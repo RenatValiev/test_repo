368494b585e4ef0bf222dea613d64fbfb843ebe8
PCuser <hitkillerman@gmail.com>
2022-06-03 01:37:18 +0300

    asd homework 3

---
+++ asd222.py
@@ -0,0 +1,54 @@
+import collections
+
+
+class Graph:
+    def __init__(self):
+        self.edges = {}
+
+    def neighbors(self, id):
+        return self.edges[id]
+
+example_graph = Graph()
+example_graph.edges = {
+    'A': ['B'],
+    'B': ['A', 'C', 'D'],
+    'C': ['A'],
+    'D': ['E', 'A'],
+    'E': ['B']
+}
+
+
+class Queue:
+    def __init__(self):
+        self.elements = collections.deque()
+
+    def empty(self):
+        return len(self.elements) == 0
+
+    def put(self, x):
+        self.elements.append(x)
+
+    def get(self):
+        return self.elements.popleft()
+
+
+def search(graph, start):
+    # печать того, что мы нашли
+    frontier = Queue()
+    frontier.put(start)
+    visited = {}
+    visited[start] = True
+
+    while not frontier.empty():
+        current = frontier.get()
+        print("Visiting %r" % current)
+        for next in graph.neighbors(current):
+            if next not in visited:
+                frontier.put(next)
+                visited[next] = True
+
+
+search(example_graph, 'A')
+
+
+
